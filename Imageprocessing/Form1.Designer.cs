0872184fab8e6f20fcf6cf84a42f4483906dcc9e
ShubhamKharote <kharoteshubham007@gmail.com>
2025-02-08 00:12:19 +0530

    Image Commit

---
+++ Imageprocessing/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Imageprocessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image Processing";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 323);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(260, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Grayscale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.Location = new System.Drawing.Point(517, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(470, 323);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(443, 458);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 52);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Apply Filter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.radioButton3.Location = new System.Drawing.Point(531, 417);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 20);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Blue";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Lime;
+            this.radioButton2.Location = new System.Drawing.Point(615, 414);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 20);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Green";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Red;
+            this.radioButton1.Location = new System.Drawing.Point(446, 414);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 20);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(677, 458);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 52);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Flip";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(773, 458);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 52);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Exp-1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(924, 458);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(174, 52);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Binarized";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(924, 417);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 37);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "60";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(993, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 37);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Color ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(998, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 37);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Recognition";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(1010, 149);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(174, 52);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Red";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(1010, 216);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(174, 52);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "Blue";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(1010, 283);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(174, 52);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "Yellow";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 748);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Imageprocessing/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imageprocessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "jpg|*.jpg|png|*.png|jpeg|*.jpeg|bmp|*.bmp|all files|*.*";
+            DialogResult dr = fd.ShowDialog();
+            if (dr == DialogResult.OK) 
+            {
+                try {
+                    pictureBox1.Image = Bitmap.FromFile(fd.FileName);
+                }catch(Exception ex) { MessageBox.Show("Image Size Too much"+ex); }
+                  }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cursor.Current= Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            for (int y=0; y < bt.Height; y++) 
+            {
+                for (int x =0; x < bt.Width; x++) 
+                {
+                    Color c =bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g =c.G;
+                    int b =c.B;
+                    
+                    int avg = (r + g + b) / 3;
+                    Color tempcolor = Color.FromArgb(avg, avg, avg);
+                    bt.SetPixel(x, y, tempcolor);
+
+                }
+            }
+            pictureBox2. Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int avg = (r + g + b) / 3;
+                  
+                        Color tempcolor = Color.FromArgb(avg, avg, avg);
+
+                    if (radioButton1.Checked) 
+                    {
+                         tempcolor = Color.FromArgb(255, avg, avg);
+                    }
+                    if (radioButton2.Checked)
+                    {
+                       tempcolor = Color.FromArgb(avg, 255, avg);
+                    }
+                    if (radioButton3.Checked)
+                    {
+                        tempcolor = Color.FromArgb(avg, avg, 255);
+                    }
+                    bt.SetPixel(x, y, tempcolor);
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            Bitmap bt2 = new Bitmap(bt.Width,bt.Height);
+            int w = bt.Width;
+            int h = bt.Height;
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    bt2.SetPixel(w-1-x, y, c);
+
+                }
+            }
+            pictureBox2.Image = bt2;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            int w = bt.Width;
+            int h = bt.Height;
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    bt.SetPixel(w - 1 - x, y, c);
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            int th = int.Parse(textBox1.Text);
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int avg = (r + g + b) / 3;
+                    if (avg < th)
+                    {
+                        bt.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        bt.SetPixel(x, y, Color.White);
+                    }
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            int th = int.Parse(textBox1.Text);
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int avg = (r + g + b) / 3;
+                    if (avg < th)
+                    {
+                        bt.SetPixel(x, y, Color.Red);
+                    }
+                    else
+                    {
+                        bt.SetPixel(x, y, Color.White);
+                    }
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            int th = int.Parse(textBox1.Text);
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int avg = (r + g + b) / 3;
+                    if (avg < th)
+                    {
+                        bt.SetPixel(x, y, Color.Blue);
+                    }
+                    else
+                    {
+                        bt.SetPixel(x, y, Color.White);
+                    }
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Bitmap bt = new Bitmap(pictureBox1.Image);
+            int th = int.Parse(textBox1.Text);
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    Color c = bt.GetPixel(x, y);
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int avg = (r + g + b) / 3;
+                    if (avg < th)
+                    {
+                        bt.SetPixel(x, y, Color.Yellow);
+                    }
+                    else
+                    {
+                    //    bt.SetPixel(x, y, Color.White);
+                    }
+
+                }
+            }
+            pictureBox2.Image = bt;
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
